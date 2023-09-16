6c08d17349ec1cc109aaec8ae66ced673eff98eb
Gavin Simpson <GDSimpson3@gmail.com>
2023-09-16 20:43:14 +0100

    added colors

--- lazygit.sh
+++ lazygit.sh
@@ -1,8 +1,13 @@
-echo "Make sure you have setup your github remote origin via \n git init \n git remote add origin https://github.com/youruser/arandomrepoidk"
+RED='\033[0;31m'
+Green='\033[0;32m'   
+Yellow='\033[0;33m' 
+Cyan='\033[0;36m'
+
+echo "${RED} Make sure you have setup your github remote origin via \n git init \n git remote add origin https://github.com/youruser/arandomrepoidk"
 git add .
-echo "Commit message?: "
+echo "${Cyan} Commit message?: "
 read commitmessage
-echo "Branch name?: "
+echo "Branch name?: ${Yellow}"
 read branchname
 git branch -m $branchname
 git commit -m "$commitmessage"
